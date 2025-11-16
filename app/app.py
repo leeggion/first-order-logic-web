6af36f7f53d4a63e076f990b4d68700f4f1c9d9a
t33nsy <krayoshikage2@gmail.com>
2025-11-16 20:46:50 +0300

    blueorints added

--- app/app.py
+++ app/app.py
@@ -1,29 +1,20 @@
-from flask import Flask, jsonify
-from db import db, User
+from flask import Flask
+from db import db
+from blueprints.main import main_bp
 
 app = Flask(__name__)
 
-# URL для PostgreSQL через docker-compose
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@db:5432/postgres"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
+# регистрируем blueprint
+app.register_blueprint(main_bp)
+
+# создаём таблицы
+with app.app_context():
     db.create_all()
-
-@app.route("/")
-def index():
-    users = User.query.all()
-    return jsonify([{"id": u.id, "name": u.name} for u in users])
-
-@app.route("/add/<name>")
-def add_user(name):
-    u = User(name=name)
-    db.session.add(u)
-    db.session.commit()
-    return {"status": "ok", "user": name}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
