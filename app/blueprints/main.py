ec8da42453f8591de44548dfc929e37425aee64a
t33nsy <krayoshikage2@gmail.com>
2025-11-18 01:43:55 +0300

    SVO removed

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from SVO import FolConverterEn
+from app.utilities.FolConvertion import FolConverterEn
 
 main_bp = Blueprint("main", __name__, template_folder="../templates")
 
